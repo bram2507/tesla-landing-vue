1944f0948f0535d4cde940650270ec9ae54f10ff
Abraham Álvarez Gómez <abrahamalvarezgomez2507@gmail.com>
2023-06-04 18:28:32 +0200

    create project and first commit

---
+++ src/components/Landing.vue
@@ -0,0 +1,27 @@
+
+<template>
+	<main
+		class="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth"
+	>
+		<div>
+			<NavbarVue />
+		</div>
+	</main>
+</template>
+
+<script>
+import NavbarVue from "./NavBar/Navbar.vue";
+
+export default {
+	components: {
+		NavbarVue,
+	},
+};
+</script>
+
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
